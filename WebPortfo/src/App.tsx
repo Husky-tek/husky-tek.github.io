2d5cce6fa5168b57f226d1d381297ed196884ea4
Husky-tek <65168644+Husky-tek@users.noreply.github.com>
2025-11-06 20:11:23 -0700

    resume download
    User can now view the resume without having to download it

--- WebPortfo/src/App.tsx
+++ WebPortfo/src/App.tsx
@@ -6,6 +6,7 @@
     import BlogPost from './components/BlogPost';
     import { Box, Card, Typography, Container } from '@mui/material';
     import Button from '@mui/material/Button'
+    import Resume from './assets/JamesCatbaganResume2025.pdf'
      
     
     const Home = () => (
@@ -23,9 +24,11 @@
       >
 
         <Card sx={{backgroundColor: 'rgba(0,0,0,0.5)', p: { xs: 2, md: 4 } }}>
-          <Typography variant='h2' sx={{ mb: 2 }}>Hello,</Typography>
-          <Typography variant='h2' >I'm James Catbagan</Typography>
-          <Button href='/downloads/JamesCatbaganResume2025.pdf' download="JamesCatbaganResume2025.pdf">Resume Download</Button>
+          <Typography variant='h2' sx={{ mb: 2 }}>Hello, I'm James Catbagan</Typography>
+          <Typography variant='body1' >I'm a CS student @ UTEP focusing on web development who is also a game dev hobbyist.</Typography>
+          <a href={Resume} target='_blank' style={{ textDecoration: 'none' }}>
+            <Button variant="contained" sx={{ mt: 2 }}>Resume Download</Button>
+          </a>
         </Card>
         
         
